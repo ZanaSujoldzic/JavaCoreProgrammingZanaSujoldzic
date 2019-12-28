b33cae38fa7c5139a4b40b7f33ed3351b3c314e7
ZanaSujoldzic <58978596+ZanaSujoldzic@users.noreply.github.com>
2019-12-28 17:39:45 +0100

    Switch; case; - without break;

---
+++ src/four/SwitchCaseLeapYear.java
@@ -0,0 +1,46 @@
+
+package four;
+
+import java.util.Scanner;
+
+public class SwitchCaseLeapYear {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Unesite broj mjeseca:");
+        int monthNumber = myScanner.nextInt();
+        System.out.println("Unesite broj godine:");
+        int yearNumber = myScanner.nextInt();
+        int daysInMonth = 0;
+        switch(monthNumber){
+            case 1:  
+            case 3:  
+            case 5:  
+            case 7:  
+            case 8:  
+            case 10:  
+            case 12:
+             daysInMonth = 31;
+             break;
+            case 4: 
+            case 6: 
+            case 9: 
+            case 11:
+             daysInMonth = 30;
+             break; 
+            case 2:
+                if((yearNumber%4 == 0 && yearNumber%100 != 0) || (yearNumber%400 == 0)){
+                    daysInMonth = 29;
+                }else{
+                    daysInMonth = 28;
+                }
+                break;
+                  
+            default:
+                System.out.println("Invalid month number.");
+                break; 
+                
+        }
+        System.out.println("Number of days in month = " + daysInMonth);
+    }
+    
+}
---
+++ src/four/SwitchStringDemonstration.java
@@ -0,0 +1,42 @@
+
+package four;
+
+import javax.swing.JOptionPane;
+
+// >= 1.7 Java SE
+
+public class SwitchStringDemonstration {
+    public static void main(String[] args) {
+        String weekName = JOptionPane.showInputDialog("Unesi ime dana u sedmici na Engleskom jeziku da saznaš prijevod:");
+        switch(weekName.toLowerCase()){
+            case "monday":
+                System.out.println("Ponedjeljak");
+                break;
+            case "tuesday":
+                System.out.println("Utorak");
+                break;
+            case "wednesday":
+                System.out.println("Srijeda");
+                break;
+            case "thursday":
+                System.out.println("Četvrtak");
+                break;
+            case "friday":
+                System.out.println("Petak");
+                break;
+            case "saturday":
+                System.out.println("Subota");
+                break;
+            case "sunday":
+                System.out.println("Nedjelja");
+                break;
+            default: 
+                System.out.println("Unio si pogrešan naziv.");
+                break;
+        
+                
+                
+        }
+    }
+    
+}
