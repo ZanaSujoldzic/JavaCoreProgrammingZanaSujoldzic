6e4ce495e5ce45090e92405dc354ee320d9655bf
ZanaSujoldzic <58978596+ZanaSujoldzic@users.noreply.github.com>
2020-01-28 23:50:47 +0100

    switch with enum

--- src/eight/Season.java
+++ src/eight/Season.java
@@ -24,7 +24,30 @@ public enum Season {
         return number;
     }
     
+    public static Season findSeasonByNumber(int number){
+        Season[] seasons = Season.values();
+        Season season = null;
+        for(int i = 0; i<seasons.length; i++){
+            Season mutant = seasons[i];
+            if(mutant.getNumber() == number){
+                season = mutant;
+                break;
+                
+            }
+            
+        }
+        
+        return season;
+    }
     
-   
+   public static Season findSeasonByName(String name){
+       Season[] seasons = Season.values();
+       for(Season season : seasons){
+           if (season != null && season.getName().toLowerCase().equals(name.toLowerCase())){
+               return season;
+           }
+       }
+       return null;
+   }
     
 }
--- src/eight/SeasonTester.java
+++ src/eight/SeasonTester.java
@@ -1,10 +1,42 @@
 
 package eight;
 
+import java.util.Scanner;
+
 public class SeasonTester {
     public static void main(String[] args) {
-        Season spring = Season.SPRING;
-        System.out.println(spring.getName());
+        Season season = Season.SPRING;
+        // System.out.println(season.getName());
+        
+        // Switch + enum
+        
+       // System.out.println("Unesite broj od 1 do 4:");
+        
+        //int numberInput = new Scanner(System.in).nextInt();
+        
+        //Season season1 = season.findSeasonByNumber(numberInput);
+        
+        System.out.println("Unesite naziv jednog godišnjeg doba");
+        String nameInput = new Scanner(System.in).nextLine();
+        Season season2 = season.findSeasonByName(nameInput);
+        
+        switch(season){
+            case AUTMN:
+                System.out.println("Lišće opada.");
+                break;
+            case SPRING:
+                System.out.println("Sve cvjeta.");
+                break;
+            case SUMMER:
+                System.out.println("Pješčana plaža, more..");
+                break;
+            case WINTER:
+                System.out.println("Zimski sportovi.");
+                break;
+            default:
+                System.out.println("Invalid month name");
+            
+        }
         
     }
     
